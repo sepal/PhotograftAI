09c6edf0d9993a153ef09d558873660eb191f2ad
Sebastian Gilits <sep.gil@gmail.com>
2023-08-21 22:22:16 +0200

    Change api route return plain npy file from zip

--- src/app/api/image/[id]/embeddings/route.tsx
+++ src/app/api/image/[id]/embeddings/route.tsx
@@ -1,5 +1,6 @@
 import { getXataClient } from "@/lib/xata";
 import { NextResponse } from "next/server";
+import JSZip from "jszip";
 
 type Params = {
   params: { id: string };
@@ -11,16 +12,21 @@ export async function GET(req: Request, { params }: Params) {
 
   const xata = getXataClient();
 
-  const file = await xata.files.download({
+  const zipBlob = await xata.files.download({
     table: "Images",
     column: "embeddings",
     record: id,
   });
 
-  const headers = new Headers();
-  headers.set("Content-Type", file.type);
+  console.log("getting buffer");
+  const buffer = await zipBlob.arrayBuffer();
+  console.log("loading zip");
+  const zip = await JSZip.loadAsync(buffer);
+  console.log("Loading tensorfile");
+  const file = await zip.file("tensor.npy")!.async("nodebuffer");
 
-  console.log(file);
+  const headers = new Headers();
+  headers.set("Content-Type", "application/octet-stream ");
 
   return new NextResponse(file, {
     status: 200,
